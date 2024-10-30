4f816b3c1f7a17fea703266e9989fbd4a633d474
YongHyeon_Park <dydgustmdfl@gmail.com>
2024-10-31 00:50:38 +0900

    add RTT측정
    # 라운드 트립 레이턴시 측정 (사실상 핑)
    
    (유저 클래스의 메서드로 클라이언트와 서버간의 RTT를 측정, 저장합니다.)
    
    - 네트워크 라운드 트립 (RTT): 클라이언트에서 서버로 요청을 보내고 응답을 받기까지 걸리는 전체 왕복 시간.
    - 레이턴시 (Latency): 사용자의 입력이 서버에 도달하고 반응이 일어나는 지연 시간으로, 네트워크 지연을 포함한 더 넓은 범위의 시간을 의미합니다.
    - 게임에서 레이턴시는 게임 내에서 느끼는 반응 속도로, 빠른 응답을 위해서는 네트워크 지연, 서버 처리 시간, 클라이언트의 그래픽 렌더링 속도 등 다양한 요소의 최적화가 필요합니다.

--- src/classes/models/user.class.js
+++ src/classes/models/user.class.js
@@ -1,3 +1,5 @@
+import { createPingPacket } from '../../utils/notification/game.notification.js';
+
 class User {
   constructor(id, socket) {
     this.id = id;
@@ -6,6 +8,8 @@ class User {
     this.y = 0;
     this.sequence = 0;
     this.lastUpdateTime = Date.now();
+    // 있어도 없어도 그만
+    this.latency = 0;
   }
 
   updatePosition(x, y) {
@@ -16,6 +20,21 @@ class User {
 
   getNextSequence() {
     return ++this.sequence;
+  }
+
+  // 보인에게 핑을 보냄
+  ping() {
+    const now = Date.now();
+
+    console.log(`${this.id}: ping`);
+    this.socket.write(createPingPacket(now));
+  }
+
+  // 서버에서 보냈으면 클라에서도 다시 반환해줌
+  handlePong(data) {
+    const now = Date.now();
+    this.latency = (now - data.timestamp) / 2;
+    console.log(`Received pong from user ${this.id} at ${now} with latency ${this.latency}ms`);
   }
 }
 
---
+++ src/utils/notification/game.notification.js
@@ -0,0 +1,32 @@
+// 게임 내에서 게임에 관련된 모든 알림을 여기서 관리
+// 서버에서 뭔가 유저에게 알림을 보낼게 있을 때
+
+import { config } from '../../config/config.js';
+import { PACKET_TYPE } from '../../constants/header.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
+
+const makeNotification = (message, type) => {
+  // 패킷 길이 정보를 포함한 버퍼 생성
+  const packetLength = Buffer.alloc(config.packet.totalLength);
+  packetLength.writeUInt32BE(
+    message.length + config.packet.totalLength + config.packet.typeLength,
+    0,
+  );
+
+  // 패킷 타입 정보를 포함한 버퍼 생성
+  const packetType = Buffer.alloc(config.packet.typeLength);
+  packetType.writeUInt8(type, 0);
+
+  // 길이 정보와 메시지를 함께 전송
+  return Buffer.concat([packetLength, packetType, message]);
+};
+
+export const createPingPacket = (timestamp) => {
+  const protoMessages = getProtoMessages();
+  const ping = protoMessages.common.Ping;
+
+  const payload = { timestamp };
+  const message = ping.create(payload);
+  const pingPacket = ping.encode(message).finish();
+  return makeNotification(pingPacket, PACKET_TYPE.PING);
+};
